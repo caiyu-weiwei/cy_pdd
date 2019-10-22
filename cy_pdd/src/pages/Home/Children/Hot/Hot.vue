42323f5907a09f5298e08e188a797e56c069fba5
weiwei.cai <weiwei.cai@zhangmen.com>
2019-10-22 20:29:32 +0800

    添加广告位

--- cy_pdd/src/pages/Home/Children/Hot/Hot.vue
+++ cy_pdd/src/pages/Home/Children/Hot/Hot.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hot">
+    <!-- 轮播图 -->
     <div class="swiper-container">
       <div class="swiper-wrapper">
         <div class="swiper-slide" v-for="(item, index) in sliderImgList" :key="index">
@@ -8,7 +9,12 @@
       </div>
       <div class="swiper-pagination"></div>
     </div>
+    <!-- 中间导航 -->
     <hot-nav />
+    <!-- 广告位 -->
+    <div class="hot-ad">
+      <img :src="hotAd" alt="">
+    </div>
   </div>
 </template>
 
@@ -33,7 +39,8 @@
           require('../../imgs/rowing/s7.png'),
           require('../../imgs/rowing/s8.png'),
           require('../../imgs/rowing/s9.png'),
-        ]
+        ],
+        hotAd: require('../../imgs/hot_ad/home_ad.gif')
       }
     },
     mounted() {
@@ -64,4 +71,7 @@
     width 100%
     height 100%
     padding-top 46px
+    .hot-ad
+      img
+        width 100%
 </style>
---
+++ cy_pdd/src/pages/Home/Children/Hot/HotShopList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hot-shop-list">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: HotShopList
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
