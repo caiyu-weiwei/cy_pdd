b98bc3ff8d04ec8b517954255649318d33998b56
weiwei.cai <weiwei.cai@zhangmen.com>
2019-10-24 11:34:51 +0800

    在axios基础上简单封装了ajax

--- cy_pdd/src/api/ajax.js
+++ cy_pdd/src/api/ajax.js
@@ -1,1 +1,30 @@
 import axios from 'axios'
+export default function ajax(url = '', params = {}, type = 'GET') {
+  let promise
+  return new Promise((resolve, reject) => {
+    if (type === 'GET') {
+      let paramsStr = ''
+      Object.keys(params).map(key => {
+        paramsStr += key + '=' + params[key] + '&'
+      })
+
+      // 去掉拼接字符串中的最后一个 &
+      paramsStr && (paramsStr = paramsStr.substring(0, paramsStr.length-1))
+
+      // 组装GET请求中的ual
+      url = url + '?' + paramsStr
+
+      // 返回axios返回的promise
+      promise = axios.get(url)
+
+    } else if (type === 'POST') {
+      promise = axios.post(params)
+    }
+
+    promise.then(response => {
+      resolve(response)
+    }).catch(error => {
+      reject(error)
+    })
+  })
+}
