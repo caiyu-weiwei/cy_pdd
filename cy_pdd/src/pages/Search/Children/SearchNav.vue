2a8f73aae72ecc58fd8c7cdadd01c77f5db650e5
weiwei.cai <weiwei.cai@zhangmen.com>
2019-10-30 16:54:22 +0800

    搜索模块的搜索导航

--- cy_pdd/src/pages/Search/Children/SearchNav.vue
+++ cy_pdd/src/pages/Search/Children/SearchNav.vue
@@ -8,22 +8,41 @@
 </template>
 
 <script>
-  import searchNavIcon from '../images/search.png'
   export default {
     name: 'SearchNav',
     data() {
       return {
-        searchNavIcon: searchNavIcon
+        searchNavIcon: require('../images/search.png')
       }
     },
   }
 </script>
 
 <style scoped lang="stylus" ref="stylesheet/stylus">
-  // @import '../../../  common/stylus/mixins.styl'
+  @import '../../../common/stylus/mixins.styl'
   .search-nav
     width 100%
     height 60px
     background #fff
-    // border-bottom-1px(red)
+    border-bottom-1px(#ccc)
+    position fixed
+    top 0
+    left 0
+    z-index 998
+    display flex
+    justify-content center
+    align-items center
+    .search-nav-icon
+      width 90%
+      height 70%
+      background-color #e8e8e8
+      border-radius 10px
+      display flex
+      justify-content center
+      align-items center
+      span
+        color #aaa
+        margin-left 6px
+        font-size 18px
+        font-weight lighter
 </style>
