4636ec168f95b455f820bb251718793acd476863
weiwei.cai <weiwei.cai@zhangmen.com>
2019-11-06 20:13:09 +0800

    热门搜索

--- cy_pdd/src/pages/Search/Children/SearchPanel.vue
+++ cy_pdd/src/pages/Search/Children/SearchPanel.vue
@@ -7,6 +7,15 @@
       </div>
       <button @click="isShowSearchPanel(false)">取消</button>
     </div>
+    <div class="search-content">
+      <div class="search-title">
+        <img :src="hotIcon" alt="">
+        <span>热门搜索</span>
+      </div>
+      <ul class="hot-list">
+        <li class="list-item" v-for="(hot, index) in hotList" :key="index">{{hot.name}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -18,7 +27,28 @@
     },
     data() {
       return {
-        searchIcon: require('../images/search.png')
+        searchIcon: require('../images/search.png'),
+        hotIcon: require('../images/hot.png'),
+        hotList: [
+          {
+            name: '面巾纸'
+          },
+          {
+            name: '浴巾儿童'
+          },
+          {
+            name: '石榴水果'
+          },
+          {
+            name: '冰箱保鲜盒'
+          },
+          {
+            name: '富豪卷纸'
+          },
+          {
+            name: '毛巾浴袍'
+          },
+        ]
       }
     },
     mounted() {
@@ -66,4 +96,30 @@
         background-color transparent
         font-size 16px
         color #aaa
+    .search-content
+      width 100%
+      margin-top 50px
+      .search-title
+        width 100%
+        height 60px
+        display flex
+        align-items center
+        font-size 16px
+        padding 0 20px
+        img 
+          width 25px
+        span
+          color #ccc
+      .hot-list
+        width 100%
+        display flex
+        flex-wrap wrap
+        .list-item
+          margin 5px 10px
+          padding 5px 10px
+          color #aaa
+          font-size 12px
+          background-color #ededed
+          border-radius 8px
+          font-weight lighter
 </style>
